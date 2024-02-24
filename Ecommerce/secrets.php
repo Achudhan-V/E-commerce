803263e6206e59383e4199daf5625a69f8a7cbde
Achudhan-V <143203413+Achudhan-V@users.noreply.github.com>
2024-02-24 15:04:08 +0530

    Delete Ecommerce/secrets.php

--- Ecommerce/secrets.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-$stripeSecretKey = 'sk_test_51OTdglSEqi8lAh8nLVpxVBQm2pJZBjGb5NcGyyJ4yFQ75un8f8gtcUVF4HFJTW82XOtOBSf5uKN2BWcmCntBieIJ00gJxWeC23';
